260f7f094dbb171d7ef62a2e4a89e0ef16cdd9e6
sarahmilli45 <135390661+sarahmilli45@users.noreply.github.com>
2023-06-03 02:30:58 +0000

    Added a farewell

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ cost_of_cable = feet_of_cable * .87
 message = f"{company_name.title()}, your total cost is: $" + str(cost_of_cable)
 print(message)
 print("Thank you for your order!")
+print("Come back soon!")
