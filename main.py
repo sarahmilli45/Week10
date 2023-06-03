0f113d6fb34655a750733c76644d24ae70a73954
sarahmilli45 <21546136-sarahmilli45@users.noreply.replit.com>
2023-06-03 02:04:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome!")
+
+# Get company name
+company_name = input("\nPlease enter your company name: ")
+# Get number of feet of cable
+feet_of_cable = input("Please enter the feet of cable: ")
+feet_of_cable = float(feet_of_cable)
+# Multiply feet by .87 for cost
+cost_of_cable = feet_of_cable * .87
+
+message = f"{company_name.title()}, your total cost is: $" + str(cost_of_cable)
+print(message)
